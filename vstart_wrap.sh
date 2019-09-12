d3f5e3ed5096a1085a0185d6933430cac29808b5
Gerald Yang <liming.yang@easystack.cn>
2019-09-12 18:46:25 +0800

    Create 2 MSD server by default

--- vstart_wrap.sh
+++ vstart_wrap.sh
@@ -1,5 +1,5 @@
 if [ "$1" = "new" ]; then
-	OSD=3 MON=3 MDS=1 MGR=1 RGW=1 ./vstart.sh -n --rgw_port 7480 --rgw_frontend civetweb -b
+	OSD=3 MON=3 MDS=2 MGR=1 RGW=1 ./vstart.sh -n --rgw_port 7480 --rgw_frontend civetweb -b
 	exit 0
 fi
-OSD=3 MON=3 MDS=1 MGR=1 RGW=1 ./vstart.sh --rgw_port 7480 --rgw_frontend civetweb -b
+OSD=3 MON=3 MDS=2 MGR=1 RGW=1 ./vstart.sh --rgw_port 7480 --rgw_frontend civetweb -b
